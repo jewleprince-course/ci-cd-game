a0faeb203b93e83cd59cb3ddb786e4de4f350ce1
satheesh <satheesh.m5465@gmail.com>
2026-04-03 07:26:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "game_server" {
   ami           = "ami-019715e0d74f695be" 
   instance_type = "t3.micro"
-  key_name      = "jenkins"
+  key_name      = "ubuntu"
 
   security_groups = [aws_security_group.gamesathe.name]
 
